a9187db4474769ec442325ecb6813d3399fc4dab
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-06-08 15:33:52 -0500

    project created with spring initializr

---
+++ src/main/java/com/sofka/back_raulStore/BackRaulStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.back_raulStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackRaulStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackRaulStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/back_raulStore/BackRaulStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.back_raulStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRaulStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
