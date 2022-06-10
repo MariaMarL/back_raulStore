2f720d5dc8a7255e38267c029e0ae12e8b1aa5e3
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-06-10 02:19:09 -0500

    ProductGetByIdUseCase created

--- src/main/java/com/sofka/back_raulStore/product/useCase/ProductGetByIdUseCase.java
+++ src/main/java/com/sofka/back_raulStore/product/useCase/ProductGetByIdUseCase.java
@@ -3,19 +3,22 @@ package com.sofka.back_raulStore.product.useCase;
 import com.sofka.back_raulStore.product.dto.ProductDto;
 import com.sofka.back_raulStore.product.mapper.ProductMapper;
 import com.sofka.back_raulStore.product.repository.ProductRepository;
+import com.sofka.back_raulStore.provider.dto.ProviderDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @AllArgsConstructor
-public class ProductGetAllUseCase {
+public class ProductGetByIdUseCase {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
 
-    public Flux<ProductDto> getProducts(){
-        return productRepository.findAll().map(productMapper::toProductDto);
-        //return productRepository.findAll().map(product -> productMapper.toProductDto(product));
+    public Mono<ProductDto> getProductById(String id){
+
+        return productRepository.findById(id)
+                .switchIfEmpty(Mono.error(()-> new IllegalStateException("Product not found. ID: "+ id)))
+                .map(productMapper::toProductDto);
     }
 }
