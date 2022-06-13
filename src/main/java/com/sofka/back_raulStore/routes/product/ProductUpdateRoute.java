345176105f395caab2306600d891fa3d6be41d32
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-06-12 20:18:37 -0500

    put useCase and route added

--- src/main/java/com/sofka/back_raulStore/routes/product/ProductUpdateRoute.java
+++ src/main/java/com/sofka/back_raulStore/routes/product/ProductUpdateRoute.java
@@ -1,34 +1,32 @@
 package com.sofka.back_raulStore.routes.product;
 
 import com.sofka.back_raulStore.dto.ProductDto;
-import com.sofka.back_raulStore.useCase.produc.ProductCreateUseCase;
-import org.springframework.context.annotation.Configuration;
+import com.sofka.back_raulStore.useCase.produc.ProductUpdateUseCase;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class ProductCreateRoute {
+public class ProductUpdateRoute {
 
     @Bean
-    public RouterFunction<ServerResponse> createProduct(ProductCreateUseCase createProduct){
-        return route(POST("/api/product/create").and(accept(MediaType.APPLICATION_JSON)),
+    public RouterFunction<ServerResponse> updateProduct (ProductUpdateUseCase updateProduct){
+        return route(PUT("/api/product/update").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(ProductDto.class)
-                        .flatMap(createProduct::createProduct)
-                        .flatMap(productDto -> ServerResponse.status(HttpStatus.CREATED)
+                        .flatMap(updateProduct::updateProduct)
+                        .flatMap(productDto -> ServerResponse.status(HttpStatus.ACCEPTED)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .bodyValue(productDto))
                         .onErrorResume(throwable -> {
                             System.out.println(throwable.getMessage());
                             return ServerResponse.status(HttpStatus.BAD_REQUEST).build();
-
-                                }
-                        ));
+                        }));
     }
 }
